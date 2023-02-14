a5d86150177e2f4ece99595ab4c3345deec6c32f
brahmbhattprexa15 <brahmbhattprexa15@gmail.com>
2023-02-14 12:11:38 +0530

    webdevlopment

---
+++ javascript/javascript/string.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Front end developer", "Backend developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+});
+});
